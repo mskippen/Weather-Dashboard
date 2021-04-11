b640488fc611ef0b43da7a260f2bef46fe9de915
Megan Skippen <79444992+mskippen@users.noreply.github.com>
2021-04-12 00:23:40 +1000

    JAVASCRIPT updated with weather functions

---
+++ Assets/js/script.js
@@ -0,0 +1,108 @@
+var form = document.querySelector("form")
+var searchInput = document.querySelector("input")
+var currentWeatherContainer = document.querySelector(".details")
+var fiveDaysWeatherContainer = document.querySelector(".results-container")
+var searchHistoryContainer = document.querySelector(".search-history")
+var API_KEY = "58ee873c9d639e1c4a248c21a4188189"
+
+
+function fetchCurrentWeather (url) {
+    fetch(url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function(data) {
+        fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=58ee873c9d639e1c4a248c21a4188189`)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(UV_data) {
+            displayCurrentWeather(data, UV_data.value)
+        })
+        fetchFiveDaysForecast( `http://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude={part}&appid=58ee873c9d639e1c4a248c21a4188189`)
+    })
+}
+
+function showUVIndexColor () {
+    var uvIndexSpan = document.querySelector("#uv-index")
+    var uvIndex = Number(uvIndexSpan.textContent)
+    console.log(typeof uvIndex)
+    
+    if(uvIndex >= 0 && uvIndex < 2) {
+        uvIndexSpan.classList.add("uv-green")
+    } else if (uvIndex >= 3 && uvIndex < 6) {
+        uvIndexSpan.classList.add("uv-yellow")
+    } else if (uvIndex >= 6 && uvIndex <= 7) {
+        uvIndexSpan.classList.add("uv-orange")
+    } else if (uvIndex >= 8) {
+        uvIndexSpan.classList.add("uv-red")
+    }
+}
+
+function displayCurrentWeather (data, UV_INDEX) {
+    console.log(data)
+    var iconcode = data.weather[0].icon
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    var time = data.dt;
+    var newTime = moment.unix(time).format("DD/MM/YYYY")
+    var output = ""
+    output = `
+    <h3>${data.name} (${newTime}) <span><img src=${iconurl} /></span></h3>
+    <ul>
+        <li>Temp: ${data.main.temp}°F</li>
+        <li>Wind: ${data.wind.speed} MPH</li>
+        <li>Humidity ${data.main.humidity}</li>
+        <li>UV index <span id="uv-index">${UV_INDEX}</span></li>
+    </ul>
+    `
+    currentWeatherContainer.innerHTML = output
+    showUVIndexColor()
+}
+
+function displayFiveDaysForecast (data) {
+    var dailyData = data.daily.slice(0, 5)
+    var output = ""
+    fiveDaysWeatherContainer.innerHTML = ""
+    dailyData.forEach(function (item) {
+        var iconcode = item.weather[0].icon
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        var time = item.dt;
+        var newTime = moment.unix(time).format("DD/MM/YYYY")
+        output = `
+        <article class="col-lg-2">
+            <ul>
+                <li>${newTime}</li>
+                <li><img src=${iconurl} /></li>
+                <li>Temp: ${item.temp.day}°F</li>
+                <li>Wind: ${item.wind_speed} MPH</li>
+                <li>Humidity ${item.humidity} %</li>
+            </ul>
+        </article>
+        `
+        fiveDaysWeatherContainer.innerHTML += output
+    })
+}
+
+function fetchFiveDaysForecast (url) {
+    fetch (url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+        displayFiveDaysForecast(data)
+    })
+ 
+}
+
+function fetchWeatherCondition (event) {
+    event.preventDefault()
+    var cityName = searchInput.value
+    var CURRENT_WEATHER_API = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+    fetchCurrentWeather(CURRENT_WEATHER_API)
+   
+}
+
+form.addEventListener("submit", function(event) {
+    fetchWeatherCondition(event)
+})
